e3404701725131b70755c83bc1f439934de20d24
Steffen Stolz <user01@DEB-SAB.dd5.home>
2019-12-24 09:12:32 +0100

    add python comparison

--- test/db_con.py
+++ test/db_con.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+ocr_result='HVL-SE 162'
 cnx = mysql.connector.connect(host='192.168.2.202', user='reader', database='home', password='reader')
 cursor = cnx.cursor()
 
@@ -10,7 +10,9 @@ cursor.execute(query)
 
 # print the first and second columns      
 for row in cursor.fetchall() :
-    print row[1]
+
+	if ocr_result == row[1]:
+		print row[1]
 
 cursor.close()
 cnx.close()
