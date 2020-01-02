8b953f9700a5dc348227d179bf35e4d0bc3597f0
stolz1982 <stolz1982@gmx.de>
2020-01-02 22:44:03 +0100

    motion detection

---
+++ test/motion_detection.py
@@ -0,0 +1,81 @@
+#https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
+# import the necessary packages
+from imutils.video import VideoStream
+import datetime
+import imutils
+import time
+import cv2
+
+min_area = 1500
+
+print(cv2.__version__)
+
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+# initialize the first frame in the video stream
+firstFrame = None
+
+# loop over the frames of the video
+while True:
+	frame = vs.read()
+	text = "No motion"
+ 
+	# if the frame could not be grabbed, then we have reached the end
+	# of the video
+	if frame is None:
+		break
+ 
+	# resize the frame, convert it to grayscale, and blur it
+	frame = imutils.resize(frame, width=500)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+ 
+	# if the first frame is None, initialize it
+	if firstFrame is None:
+		firstFrame = gray
+		continue
+	
+	# compute the absolute difference between the current frame and
+	# first frame
+	frameDelta = cv2.absdiff(firstFrame, gray)
+	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+ 
+	# dilate the thresholded image to fill in holes, then find contours
+	# on thresholded image
+	thresh = cv2.dilate(thresh, None, iterations=2)
+	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+ 
+	# loop over the contours
+	for c in cnts:
+		# if the contour is too small, ignore it
+		if cv2.contourArea(c) < min_area:
+			continue
+ 
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+		(x, y, w, h) = cv2.boundingRect(c)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		text = "Motion detected"
+
+
+	# draw the text and timestamp on the frame
+	cv2.putText(frame, "Status: {}".format(text), (10, 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+	cv2.putText(frame, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 0, 0), 1)
+
+	cv2.imshow("Security Feed", frame)
+	cv2.imshow("Thresh", thresh)
+	cv2.imshow("Frame Delta", frameDelta)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key is pressed, break from the lop
+	if key == ord("q"):
+		break
+
+# cleanup the camera and close any open windows
+vs.stop()
+cv2.destroyAllWindows()
--- test/read_cam.py
+++ test/read_cam.py
@@ -3,24 +3,36 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+
+ret, img = cap.read()
+print(img.shape)
+
+y=200
+x=200
+h=150
+w=150
+crop = img[y:y+h, x:x+w]
+cv2.imshow('Image', crop)
+print(crop.shape)
+print(crop.size)
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
+    #frame[0:0, 50:50] = rect   
     #rechteck einblenden
-    frame = cv2.rectangle(frame,(150,150),(300,300),(255,0,0),2)
+    #cv2.rectangle(was soll mit einem Rechteck versehen werden,x und y Korridaten in px und als letztes die Rahmen stärke in px
+    frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),1)
+    frame = cv2.rectangle(frame,(1,1),(10,10),(0,0,255),-1)
     
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-
     # Display the resulting frame
-
-    #cv2.imshow('frame',gray)
+    
     cv2.imshow('UGLY',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
