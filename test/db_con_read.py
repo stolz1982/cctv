8d200af57a8bfeba32642260ec5ec621e79e0775
stolz1982 <stolz1982@gmx.de>
2020-01-08 18:56:14 +0100

    gitignore und json secret import

--- test/db_con_read.py
+++ test/db_con_read.py
@@ -1,4 +1,20 @@
 import mysql.connector
+import json
+
+credentials = '/home/pi/cctv/test/credentials.secret'
+
+with open(credentials) as file:
+    data = json.load(file)
+    hostname = data["host"]
+    password = data["password"]
+    user = data["user"]
+    file.close()
+
+print(hostname)
+print(password)
+print(user)
+
+
 ocr_result='HVL-SE 162'
 cnx = mysql.connector.connect(host='192.168.2.202', user='reader', database='home', password='reader')
 cursor = cnx.cursor()
@@ -11,7 +27,7 @@ cursor.execute(query)
 # print the first and second columns      
 for row in cursor.fetchall() :
 	if ocr_result == row[1]:
-		print row[1]
+		print(row[1])
 
 cursor.close()
 cnx.close()
